715388dd2c497d404ec6f481c64ba487556b1dc6
javohir12312 <abduxalilovjavohir393@gamil.com>
2023-06-12 15:22:28 +0500

    finished

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ const startBox = document.querySelector(".startBox")
 const drop = document.querySelector(".drop")
 const wave = document.querySelector(".wave")
 const body = document.body;
+const icon = document.querySelector(".header_icon")
+const modal = document.querySelector(".modalBg")
+const closeModal = document.querySelector(".closeModal")
+const ham = document.querySelector(".ham")
+const hamOPen = document.querySelector(".hamburger")
 
+modal.style.top = "-150%";
 let enterUser = false;
 body.style.overflow = 'hidden'
 
@@ -28,3 +34,23 @@ if (localStorage.getItem("Enter")) {
 setTimeout(() => {
   localStorage.clear
 }, 36000);
+
+icon.addEventListener("click", () => {
+  modal.style.top = "0%";
+  body.style.overflow = 'hidden'
+})
+
+closeModal.addEventListener("click", () => {
+  modal.style.top = "-120%"
+  body.style.overflow = 'scroll'
+})
+
+ham.addEventListener("click", () => {
+  if (hamOPen.style.top == "10px") {
+    hamOPen.style.right = "-100%"
+    hamOPen.style.top = "0"
+  }else{
+    hamOPen.style.right = "0px"
+    hamOPen.style.top = "10px"
+  }
+})
